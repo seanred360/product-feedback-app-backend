b7e12c1c5ea5f4eeb72ebd17d94703483406a39b
Sean Redmon <seen360@yahoo.com>
2022-02-24 12:23:38 -0500

    v 1.0.0

---
+++ server.js
@@ -0,0 +1,19 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv/config");
+
+mongoose.connect(process.env.MONGO_URL, () => console.log("connected to db"));
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/posts", require("./routes/posts"));
+
+app.get("/", (req, res) => {
+  res.send("We are on home");
+});
+
+server.listen(5000, () => {
+  console.log("Server is listening at port 5000");
+});
